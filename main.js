2985a47087037eec1b381e66b3d1b1180ac86fb9
Yavuz Efe <75783774+Yefee8@users.noreply.github.com>
2022-02-13 22:20:52 +0300

    Renard v2.0 Demo first files 🚀🚀🚀

--- main.js
+++ main.js
@@ -30,20 +30,22 @@ function useMoveSys(id, width, speed, leftLimit, rightLimit){
             return;
         }
         else if(e.key === 'a'){
-            if(a){
-                if(leftLimit <= pot){
-                    return;
+            setTimeout(()=>{
+                if(a){
+                    if(leftLimit >=  pot - speed){
+                        return;
+                    }
+                    count--;
+                    setTimeout(()=>{
+                        window.scrollTo(count * width, 0);
+                    })
+                    document.getElementById(id).style.transform = `translate3d(${speed * count}px, 0, 0`
+                    a = false
+                    setTimeout(()=>{
+                        a = true
+                    },200)
                 }
-                count--;
-                setTimeout(()=>{
-                    window.scrollTo(count * width, 0);
-                })
-                document.getElementById(id).style.transform = `translate3d(${speed * count}px, 0, 0`
-                a = false
-                setTimeout(()=>{
-                    a = true
-                },200)
-            }
+            })
         }
     });
 }
